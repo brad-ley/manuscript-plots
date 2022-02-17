8fa8d55468c85676d0568a4de3867ea1d6baeb0d
Brad P <bdprice@ucsb.edu>
2022-02-17 10:47:22 -0800

    making some appearance chagnes

--- manuscriptPlots.py
+++ manuscriptPlots.py
@@ -21,12 +21,11 @@ this code will assume your data is a .txt, .dat, or .csv file with the x-axis in
 """
 CHANGE STUFF BELOW
 """
-FOLDER = '/Volumes/GoogleDrive/My Drive/Research/Data/2022/1/compare single double' 
-# legend_names = ['line 1', 'line 2'] # add as many legend names as you want to be plotted on same x axis
+FOLDER = '/Volumes/GoogleDrive/My Drive/Research/Data/2022/1/compare light on off' 
+MUTANT = ''
+FIELDS = [8.623, 8.625, 8.627, 8.628] # leave this list empty if you don't want field markers
 legend_names = ['line 1'] # add as many legend names as you want to be plotted on same x axis
-# colors = ['red','black'] # as many color names as legend names
 colors = ['red'] # as many color names as legend names
-# styles = [':', "--"] # same number here
 # for light on I use color '#00A7CA' and style '--'
 styles = [':'] # same number here
 x_Label = 'sample x label' # x axis label
@@ -60,7 +59,6 @@ def main(folder):
         data = data[np.logical_and(data[:, 0] >= lower_limit, data[:, 0] < upper_limit)]
         cols = np.shape(data)[1]
 
-
         try:
             print(f"Plot number {j + 1} is {f.stem}, label={legend_names[idx]}, color={colors[idx]}, style={styles[idx]}")
         except:
@@ -68,13 +66,33 @@ def main(folder):
 
         for i in range(1, cols):
             try:
-                plt.plot(data[:, 0], data[:, i], label=legend_names[idx], color=colors[idx], linestyle=styles[idx], lw=lw)
+                ax.plot(data[:, 0], data[:, i], label=legend_names[idx], color=colors[idx], linestyle=styles[idx], lw=lw)
             except IndexError:
-                plt.plot(data[:, 0], data[:, i], lw=lw, label='no label')
+                ax.plot(data[:, 0], data[:, i], lw=lw, label='no label')
             idx += 1
 
-    plt.xlabel(x_Label)
-    plt.ylabel(y_Label)
+    if FIELDS:
+        alphabet = 'abcdefghijklmnopqrstuvwxyz'
+        alphas = np.linspace(0.25,0.75,len(FIELDS))
+        FIELDS.sort()
+        ticks = ax.get_ylim()
+        n = 1.175
+        fact = 0.99
+        if np.min(ticks) < 0:
+            ax.set_ylim(bottom=np.min(ticks)*n)
+        else:
+            ax.set_ylim(bottom=np.min(ticks)/n)
+            fact = 1/fact
+        t = ax.get_ylim()
+        for i, f in enumerate(FIELDS):
+            ax.axvline(x=f, c='dimgray',
+                       alpha=alphas[i], lw=lw) # , label=f'$B_0^{{({alphabet[i]})}}$')
+            ax.text(f, fact*np.min(t), f'$B_0^{{({alphabet[i]})}}$', horizontalalignment='center', verticalalignment='bottom')        
+
+    ax.text(0.45, 0.51, f'$T=294$ K\n{MUTANT}',
+            horizontalalignment='left', verticalalignment='center', transform=ax.transAxes)
+    ax.set_xlabel(x_Label)
+    ax.set_ylabel(y_Label)
 
     if not x_ticks:
         ax.set_xticks([])
@@ -89,7 +107,7 @@ def main(folder):
         ax.set_yticklabels([])
 
     if legend:
-        plt.legend()
+        ax.legend()
 
     plt.savefig(P(FOLDER).joinpath(f"{savename}.tif"),dpi=300)
     plt.savefig(P(FOLDER).joinpath(f"{savename}.png"),dpi=300)
--- timeDepMultiple.py
+++ timeDepMultiple.py
@@ -20,16 +20,17 @@ plt.rcParams['font.family'] = 'sans-serif'
 """
 CHANGE STUFF BELOW (copy full path to folder by holding 'option' on Mac)
 """
-FOLDER = '/Users/Brad/Downloads/test/t'
-turn_on = 55
-turn_off = 90
-total_experiment_time = 800 # only needs to be specific for averaging
-colors = [['slategray', 'orange'], ['black', 'red']] # make one entry for each line and it's fit [[line, fit], [line, fit]]
-styles = [['-', '-.'], ['-', '--']] # make one entry for each line and it's fit [[line, fit], [line, fit]]
+FOLDER = '/Volumes/GoogleDrive/My Drive/Research/Data/2022/1/compare single double'
+MUTANT = 'T406C' # put the mutant here, it will be placed on the figure
+turn_on = 0
+turn_off = 5
+total_experiment_time = 180 # only needs to be specific for averaging
+colors = [['purple', 'red'], ['black', 'red'], ['red', 'red']] # make one entry for each line and it's fit [[line, fit], [line, fit]]
+styles = [[':', '-.'], ['-.', '-.'], ['-','-.']] # make one entry for each line and it's fit [[line, fit], [line, fit]]
 savename = 'compared'
 source = 'Light' # use 'Light' in Shiny's lab and 'Laser' in Brad's
-delimiter = '\t'
-skiprows = 4
+delimiter = ', '
+skiprows = 0
 """
 CHANGE STUFF ABOVE
 """
@@ -43,12 +44,16 @@ def show(folder):
     if P(folder).is_file():
         folder = P(folder).parent
     data_suffixes = ['.txt', '.dat', '.asc', '.csv']
-    files = [ii for ii in P(folder).iterdir() if ii.suffix in data_suffixes]
+    files = [ii for ii in P(folder).iterdir() if ii.suffix in data_suffixes if ii.name != 'fitdata.txt']
     fig, ax = plt.subplots()
 
     smoothlen = 2000
     scale = 0
     files.sort()
+    files.insert(0, files.pop())
+    files.insert(1, files.pop())
+    lines = {}
+
     for i, f in enumerate(files):
         data = np.loadtxt(f, delimiter=delimiter, skiprows=skiprows)
         data = data[np.logical_not(np.isnan(data[:, 1]))]
@@ -61,10 +66,12 @@ def show(folder):
         """
         MIGHT NEED TO CHANGE
         """
+
         while loops < int(np.round(data[-1, 0] / total_experiment_time)):
             loopdat = data[np.logical_and(data[:,0] > loops * total_experiment_time, data[:, 0] < (loops + 1) * total_experiment_time)]
             fx = interp1d(loopdat[:, 0], loopdat[:, 1]) 
             smootht = np.linspace(loopdat[0,0],loopdat[-1,0],smoothlen)
+
             if loops == 0:
                 plott = np.copy(smootht)
             expts[:,loops] = fx(smootht)
@@ -85,10 +92,15 @@ def show(folder):
                 scale = popt[1]
 
             try:
-                ax.plot(plott, dat/scale, label=f.stem.replace("_"," "),
-                        lw=lw, color=colors[i][0], linestyle=styles[i][0])
-                ax.plot(plott[plott > turn_off], ex/scale,
-                        label=rf"$\tau={popt[2]:.1f}\pm$" + f"{sd2:.1f} s", lw=lw, color=colors[i][1], linestyle=styles[i][1])
+                lines[f.stem + ' data'] = dat 
+                lines[f.stem + ' tau'] = popt[2]
+                lines[f.stem + ' 95'] = sd2
+                # ax.plot(plott, dat/scale, label=f.stem.replace("_"," "),
+                #         lw=lw, color=colors[i][0], linestyle=styles[i][0])
+                # ax.plot(plott[plott > turn_off], ex/scale,
+                #         label=rf"$\tau={popt[2]:.1f}\pm$" + f"{sd2:.1f} s", lw=lw, color=colors[i][1], linestyle=styles[i][1])
+                # ax.plot(plott[plott > turn_off], ex/scale,
+                #         lw=lw, color=colors[i][1], linestyle=styles[i][1])
             except IndexError:
                 ax.plot(plott, dat/scale, label=f.stem.replace("_"," "),
                         lw=lw)
@@ -96,18 +108,26 @@ def show(folder):
                         label=rf"$\tau={popt[2]:.1f}\pm$" + f"{sd2:.1f} s", lw=lw)
         except RuntimeError:
             print(f"{f} file did not work.")
+    
+    outstr = ""
+    for i, f in enumerate(files):
+        ax.plot(plott, lines[f.stem + ' data']/scale, label=f.stem.replace("_", " "), lw=lw, color=colors[i][0], linestyle=styles[i][0])
+        outstr += f"{f.name} fit is {lines[f.stem + ' tau']:.3f} plus/minus {lines[f.stem + ' 95']:.3f} s\n"
 
     plt.axvspan(turn_on, turn_off, color='#00A7CA', label=f"{source} on")
 
+    ax.text(0.45, 0.51, f'$T=294$ K\n{MUTANT}',
+            horizontalalignment='left', verticalalignment='center', transform=ax.transAxes)
     # handles, labels = plt.gca().get_legend_handles_labels()
     # labels.insert(0, labels.pop())
     # handles.insert(0, handles.pop())
     # plt.legend(handles, labels)
     plt.legend()
-    ax.set_ylabel('Intensity (arb. u)')
+    ax.set_ylabel('cwEPR signal at $B_0$ (arb. u)')
     ax.set_xlabel('Time (s)')
     plt.savefig(P(folder).joinpath(f'{savename}.tif'),dpi=300)
     plt.savefig(P(folder).joinpath(f'{savename}.png'),dpi=300)
+    P(folder).joinpath(f'fitdata.txt').write_text(outstr)
 
 
 if __name__ == "__main__":
